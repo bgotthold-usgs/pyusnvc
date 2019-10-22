68b40ee996698203a42b45b2c38d694d2b7bee6f
Ben Gotthold <bgotthold@usgs.gov>
2019-10-22 07:54:03 -0600

    get path right

--- pyusnvc/process.py
+++ pyusnvc/process.py
@@ -1,33 +1,34 @@
+import os
 from usnvc import *
 
 
-# # # # # # # # TO RUN THIS FILE LOCALLY UNCOMMENT BELOW # # # # # # # # #
-# # See readme for more details.
-# path = '../'
-# file_name = 'NVC v2.03 2019-03.db'
+# # # # # # # TO RUN THIS FILE LOCALLY UNCOMMENT BELOW # # # # # # # # #
+# See readme for more details.
+path = '../'
+file_name = 'NVC v2.03 2019-03.zip'
 
 
-# def send_final_result(obj):
-#     print(json.dumps(obj))
+def send_final_result(obj):
+    print(json.dumps(obj))
 
 
-# def send_to_stage(obj, stage):
-#     globals()['process_{}'.format(stage)](path, file_name,
-#                                           ch_ledger(), send_final_result,
-#                                           send_to_stage, obj)
+def send_to_stage(obj, stage):
+    globals()['process_{}'.format(stage)](path, file_name,
+                                          ch_ledger(), send_final_result,
+                                          send_to_stage, obj)
 
 
-# class ch_ledger:
-#     def log_change_event(self, change_id, change_name, change_description,
-#                          function_name, source, result):
-#         print('\n\n\n', change_id, change_name, change_description,
-#               function_name, source, result, '\n\n\n')
+class ch_ledger:
+    def log_change_event(self, change_id, change_name, change_description,
+                         function_name, source, result):
+        print('\n\n\n', change_id, change_name, change_description,
+              function_name, source, result, '\n\n\n')
 
 
-# def main():
-#     process_1(path, file_name, ch_ledger(),
-#               send_final_result, send_to_stage, None)
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+def main():
+    process_1(path, file_name, ch_ledger(),
+              send_final_result, send_to_stage, None)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
 # The first processing stage.
@@ -35,7 +36,9 @@ from usnvc import *
 #  stage for further processing.
 # It returns count which is the number of rows created by this stage.
 def process_1(path, file_name, ch_ledger, send_final_result,
-              send_to_stage, previous_stage_result):  
+              send_to_stage, previous_stage_result):
+
+    file_name = file_name.replace('.zip','.db')
     count = 0
     for element_global_id in all_keys(path + file_name):
         send_to_stage({'element_global_id': element_global_id}, 2)
@@ -43,7 +46,6 @@ def process_1(path, file_name, ch_ledger, send_final_result,
         if(count == 10):  # testing
             return 10
     return count
-        # print(pyusnvc.usnvc.cache_unit(element_global_id, file_name=file_name))
 
 
 # The second processing stage used the previous_stage_result and sends
@@ -51,6 +53,8 @@ def process_1(path, file_name, ch_ledger, send_final_result,
 # It returns 1
 def process_2(path, file_name, ch_ledger, send_final_result,
               send_to_stage, previous_stage_result):
+
+    file_name = file_name.replace('.zip','.db')
     element_global_id = previous_stage_result['element_global_id']
     process_result = cache_unit(element_global_id, file_name=path + file_name)
 
