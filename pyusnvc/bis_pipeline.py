265beed08dd6c9ce88234cd19d761bfb3aea3c41
Ben Gotthold <bgotthold@usgs.gov>
2020-01-03 09:12:34 -0700

    code should be aware of the file name since user can now add shource uri

--- pyusnvc/bis_pipeline.py
+++ pyusnvc/bis_pipeline.py
@@ -10,6 +10,8 @@ import json
 from pyusnvc.usnvc import *
 
 version = 2.03
+file_name = "NVC v2.03 2019-03.db"
+
 json_schema = None
 try:
     with open(os.path.join(os.path.dirname(__file__), './usnvc_unit_schema_2.03.json'), 'r') as json_schema_file:
@@ -22,7 +24,7 @@ except FileNotFoundError as e:
 # # file should exist here
 # get_source_item()
 # path = './'
-# file_name = 'NVC v2.03 2019-03.zip'
+# file_name = 'NVC v2.03 2019-03.db'
 # version = 2.03
 # # with open(os.path.join(os.path.dirname(__file__), './resources/usnvc_unit_schema_2.03.json'), 'r') as json_schema_file:
 # #     json_schema = json.load(json_schema_file)
@@ -33,7 +35,7 @@ except FileNotFoundError as e:
 
 
 # def send_to_stage(obj, stage):
-#     globals()['process_{}'.format(stage)](path, file_name,
+#     globals()['process_{}'.format(stage)](path,
 #                                           ch_ledger(), send_final_result,
 #                                           send_to_stage, obj)
 
@@ -46,7 +48,7 @@ except FileNotFoundError as e:
 
 
 # def main():
-#     process_1(path, file_name, ch_ledger(),
+#     process_1(path, ch_ledger(),
 #               send_final_result, send_to_stage, None)
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -55,10 +57,8 @@ except FileNotFoundError as e:
 # It creates 1 final result and many other results that it sends to the next
 #  stage for further processing.
 # It returns count which is the number of rows created by this stage.
-def process_1(path, file_name, ch_ledger, send_final_result,
+def process_1(path, ch_ledger, send_final_result,
               send_to_stage, previous_stage_result):
-
-    file_name = file_name.replace('.zip', '.db')
     count = 0
     for element_global_id in all_keys(path + file_name):
         send_to_stage({'element_global_id': element_global_id}, 2)
@@ -69,10 +69,9 @@ def process_1(path, file_name, ch_ledger, send_final_result,
 # The second processing stage used the previous_stage_result and sends
 #  a singe document to be handled as a final result.
 # It returns 1
-def process_2(path, file_name, ch_ledger, send_final_result,
+def process_2(path, ch_ledger, send_final_result,
               send_to_stage, previous_stage_result):
 
-    file_name = file_name.replace('.zip', '.db')
     element_global_id = previous_stage_result['element_global_id']
     process_result = build_unit(
         element_global_id, source_data_filename=path + file_name, version_number=version, change_log_function=ch_ledger.log_change_event)
